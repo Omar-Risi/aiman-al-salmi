cf5baaa7e7dfff27ab007a503646ce0fb75aeb11
Reesi <reesiomar@gmail.com>
2025-11-09 19:12:22 +0400

    feat(add): new ui

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -10,12 +10,14 @@ export function LinkButton({ icon, text, href }: LinkButtonProps) {
   return (
     <a
       href={href}
-      className="flex items-center gap-3 w-full p-3 bg-gray-100 hover:bg-gray-200 transition-colors rounded-lg text-gray-800 text-sm font-medium"
+      className="flex flex-col items-center gap-2 text-gray-800"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="text-gray-600">{icon}</div>
-      <span>{text}</span>
+      <div className="p-2 bg-amber-400 hover:bg-amber-500 transition-colors rounded-full flex items-center justify-center text-white">
+        {icon}
+      </div>
+      <span className="text-xs font-medium text-center">{text}</span>
     </a>
   )
 }
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,9 +1,9 @@
 import { LinkButton } from "./LinkButton"
-import { Mail, Youtube, Instagram, Linkedin, Phone, Download } from "lucide-react"
+import { Mail, Youtube, Instagram, Linkedin, Phone, Download, Smartphone } from "lucide-react"
 
 export function ProfileCard() {
   return (
-    <div className="bg-white rounded-2xl shadow-lg p-8">
+    <div className="flex flex-col items-center bg-white rounded-2xl shadow-lg p-8">
       {/* Profile Image */}
       <div className="flex justify-center mb-6">
         <div className="w-32 h-32 rounded-full overflow-hidden bg-gray-800">
@@ -13,33 +13,40 @@ export function ProfileCard() {
 
       {/* Name */}
       <h1 className="text-xl font-semibold text-gray-900 text-center mb-8">Ayman Mohammed Al Salmi</h1>
+      <p className="text-center text-gray-500 max-w-64">
+        Public relations and Stakeholder Engagement,
+        Entrepreneurship and Business Plans, Communication and Media, Engineering and Sciences, Big Data and Data Science,
+        Logistics, FinTech, Artificial Intelligence, Market Research.
+      </p>
 
       {/* Contact Buttons */}
-      <div className="space-y-3">
+      <div className="space-y-3 mt-4 w-full">
         <a
           href="/contact.vcf"
-          className="flex items-center gap-3 w-full p-3 bg-gray-100 hover:bg-gray-200 transition-colors rounded-lg text-gray-800 text-sm font-medium"
+          className="flex justify-center items-center gap-3  p-3 bg-amber-400 hover:bg-amber-300 transition-colors rounded-full w-full text-white text-sm font-medium"
           download="OmarReesi.vcf"
         >
-          <div className="text-gray-600">
+          <div className="text-white">
             <Download />
           </div>
           <span> Save contact </span>
         </a>
+      </div>
+      <div className="mt-8 grid grid-cols-3 gap-8">
         <LinkButton
           href="mailto:aimanalsalmi@gmail.com"
-          text="aimanalsalmi@gmail.com"
+          text="Gmail"
           icon={<Mail />}
         />
         <LinkButton
           href="mailto:aimanalsalmi@hotmail.com"
-          text="aimanalsalmi@hotmail.com"
+          text="Hotmail"
           icon={<Mail />}
         />
         <LinkButton
           href="tel:+96892111093"
-          text="Call 92111093"
-          icon={<Phone />}
+          text="Mobile"
+          icon={<Smartphone />}
         />
         <LinkButton
           href="https://wa.me/+96892111093"
@@ -57,6 +64,8 @@ export function ProfileCard() {
           text="Instagram"
           icon={<Instagram />}
         />
+
+        <div></div>
 
         <LinkButton
           href="https://www.linkedin.com/in/aiman-mohammed-alsalmi-3883ba24b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
